32fa2d69d10f1edee38ae2d21dfc1be0f4afcd54
Moses Huang <hhuang5@gmail.com>
2014-09-27 18:26:27 -0400

    Changed things to log probabilities to prevent numerical underflow

--- Infer.hs
+++ Infer.hs
@@ -10,14 +10,12 @@ fchoose :: Probability -> a -> a -> F a
  -- `choose p left right` mixes two values (or two distributions),
  -- taking the left with probability p and right with probability 1-p.
 ------------ transformers -----------------------
-pmap :: (a -> b) -> F a -> F b
-pfilter :: (a -> Bool) -> F a -> F a -- ^ conditional probability
+
+
 pfilterMap :: (a -> Maybe b) -> F a -> F b
 pfilterMap f = pmap fromJust . pfilter isJust . pmap f
 ------------ combining forms ----------------------
-bindx :: F a -> (a -> F b) -> F (a, b)
- -- `bindx d k` produces a joint distribution of a's and b's where
- -- the distribution of b's for any given a is given by `k a`.
+
 ------------- observers ------------------------- 
 
 
@@ -38,35 +36,71 @@ import Data.List
 
 -- Modifying this from Jayme's code
 type Probability = Double
+type LogProb = Double
+
+--------------- Helpers  -------------------
+codeProb :: Probability -> LogProb
+-- ^ to prevent underflow, we can encode prob using natural log function
+codeProb p = log p
+
+decodeProb :: LogProb -> Probability
+-- ^ Once we need an actual probability, we apply the inverse log 
+decodeProb lp = exp lp
 
 -- Formal representation of Probability distribution
 -- Modified from Karl/Jayme's code
-data P a = Dist [(a, Probability)] deriving( Show, Eq )
+data P a = Dist [(a, LogProb)] deriving( Show, Eq )
+
+-- Must have operational procedure that Distributions can only be 
+-- combined using summation, not multiplication since it is the Log
+-- probabilities that are stored
 
 ----------------- Constructors ---------------------
 certainly :: a -> P a
-certainly xs = Dist [(xs, 1.0)]
+certainly xs = Dist [(xs, codeProb 1.0)]
 
 equally :: [a] -> P a
 equally xs = 
-  Dist (zip xs (replicate 
-                (length xs) 
-                ((/) 1.0 (realToFrac (length xs)))))
+  Dist (zip xs (map codeProb
+                        (replicate 
+                         (length xs) 
+                         ((/) 1.0 (realToFrac (length xs))))))
 
-normWeighted :: (Real w) => [(w, a)] -> P a
+normWeighted :: (Real w) => [(a, w)] -> P a
 -- ^ Takes a weighted combination of 'a' and produces a
 -- normalized probability distribution based on the weights
 normWeighted xs = 
-    let denom = realToFrac $ sum (map fst xs)
-        probs = map (/ denom) (map realToFrac (map fst xs))
-        as = map snd xs
+    let denom = realToFrac $ sum (map snd xs)
+        probs = map codeProb (map (/ denom) (map realToFrac (map snd xs)))
+        as = map fst xs
     in 
       Dist (zip as probs)
+
 
 --------------- Transformers -----------------------
 pmap :: (a -> b) -> P a -> P b
 pmap f (Dist dist1) = 
     Dist (zip (map f (map fst dist1)) (map snd dist1))
+
+
+pfilter :: (a -> Bool) -> P a -> P a 
+-- ^ conditional probability, renormalized to sum to 1
+pfilter pred (Dist dist) = normWeighted [ xs | xs <- dist, pred (fst xs) ]
+
+--------------- Combining Forms  -------------------
+bindx :: P a -> (a -> P b) -> [Probability] -- P (a,b)
+-- ^ `bindx d k` produces a joint distribution of a's and b's where
+-- the distribution of b's for any given a is given by `k a`
+bindx (Dist dist1) f = 
+    let --combo = pmap f (Dist dist1)     -- Joint
+        -- combo :: P (P b)
+        -- alist = map fst dist1
+        probs = map snd dist1
+    in
+      probs
+-- collapse
+
+
 
 --------------- Observers --------------------------
 outcomeProb :: (Eq a) => P a -> a -> Probability 
@@ -80,13 +114,14 @@ outcomeProb (Dist dist) out = checkNil (Dist dist)
               in 
                 -- In case outcome is not in the set
                 case a of [] -> 0.0
-                          (y:_) -> snd $ head a
+                          (y:_) -> decodeProb (snd $ head a)
     
 eventProb :: (Eq a) => (a -> Bool) -> P a -> Probability 
 -- ^ probability of an Event, which matches the given predicate
 eventProb pred (Dist dist) = checkNil (Dist dist)
     where checkNil (Dist []) = 0
-          checkNil (Dist (y:_)) = sum [snd x | x <- dist, pred (fst x)]
+          checkNil (Dist (y:_)) = 
+              sum [decodeProb (snd x) | x <- dist, pred (fst x)]
 
 support :: P a -> [a]
 -- ^ list including every value of type a that has nonzero probability          
@@ -96,96 +131,4 @@ expected :: (a -> Double) -> P a -> Double
 -- ^ the expected value of the given function
 expected f (Dist dist) = 
     sum (zipWith (*) (map f (map fst dist))
-                     (map snd dist))
-
-{-
--- Part (B)
-throw :: D -> P Int
-throw d = (SingleDie d)
-
--- Count # of instances of x in list of xs
-count :: (Eq a) => a -> [a] -> Int
-count x xs = length (filter (\x' -> x' == x) xs)
-
--- P(Sum = sum | D1 = d1, D2 = d2)
-throw2 :: D -> D -> Int -> Float
-throw2 (D d1) (D d2) sum =
-  let allsums = (concat (map (\y -> (map (+y) [1..d1])) [1..d2])) in
-  ((/) (realToFrac (count sum allsums)) (realToFrac (length allsums)))
-
--- Part (C)
-draw :: D -> Bag -> Float
-draw d (Bag xs)
-  | xs == [] = 0.0
-  | otherwise = 
-    ((/) c s)
-    where
-      s = realToFrac (sum (map fst xs))  -- number of dice in the bag
-      dice = (map snd xs)                -- list of dice
-      Just i = elemIndex d dice          -- index of the die in the bag
-      c = realToFrac (fst (xs !! i))     -- number dice with d-sides in the bag
-  
-
--- Probability of drawing a d1-sided die and a d2-sided die from the given bag
--- P(D1 = d1 and D2 = d2) = P(D1 = d1 | D2 = d2) * P(D2 = d2)
--- Drawing with replacement, so draws from bag are independent
-draw2 :: D -> D -> Bag -> Float
-draw2 d1 d2 bag =
-  ((*) (draw d1 bag) (draw d2 bag))
-
--- Part (D)
-joint :: D -> D -> Int -> Bag -> Float
-joint d1 d2 s bag = (draw2 d1 d2 bag) * (throw2 d1 d2 s)
-
--- Computes the number of ways an m-sided and an n-sided die can sum to s
-countSums :: Int -> Int -> Int -> Int
-countSums m n s
-  | m > n = countSums n m s  -- re-order terms such that n >= m
-  | (m >= s) && (n >= s) = s
-  | (m < s) && (n >= s) = m
-  | n + m < s = 0
-  | (m < s) && (n < s) = n+m-s+1
-  | otherwise = error "invalid countSums input?"
-
--- Part (E)
-cndtnl :: D -> D -> Int -> Bag -> Float
-cndtnl (D d1) (D d2) sum (Bag bag) =
-  let dice = map snd bag
-      pairs = [Pair x y | x <- dice, y <- dice]
-      f1 = (filter (\ (Pair (D a) (D b)) -> ((a+b) >= sum)) pairs) -- filter out pairs which can't add to sum
-      f2 = [(count x f1,x) | x <- (nub f1)] -- compress duplicate pairs
-      f3 = [ (n,countSums a b sum) | (n,Pair (D a) (D b)) <- f2]
-      denom = foldr (+) 0 (map (\ (a,b) -> a*b) f3) -- # of ways possible to roll sum given bag
-      numer = countSums d1 d2 sum -- # of ways to roll sum given d1 and d2
-  in (rTF numer) / (rTF denom)
-
---countTally :: Int -> Int -> Int -> Int
-
-choose n 0 = 1
-choose 0 k = 0
-choose n k = choose (n-1) (k-1) * n `div` k
-
--- Computes the number of ways an m-sided and an n-sided die can sum to greater than s
-countGTSums :: Int -> Int -> Int -> Int
-countGTSums m n s
-  | n + m < s = 0
-  | m > n = countGTSums n m s -- re-order terms such that n >= m
-  | (m >= s) && (n >= s) = (n-2*s+m)*m - (countSums m n s)
-  | (m < s) && (n >= s) = (n-s-1)*m + (div (m*(m-1)) 2)
-  | (m < s) && (n < s) = div (((^) (n+m-s+1) 2) - (countSums m n s)) 2
-
-{-|
--- Part (F)
-RTally :: D -> D -> Int -> Int -> Bag -> Float
-RTally (D d1) (D d2) tallies trials bag =
-  let
-    bin_coeff = (choose trials tallies)
-    p1 = (^^) (countSums ) tallies
-  in
-    returnval
-
---RTally :: D -> D -> Int -> Int -> Bag -> Float
---RTally d1 d2 tallies trials =
---  let pdice = (draw2 d1 d2 bag) -}
-
--}
+                     (map decodeProb (map snd dist)))
--- dice.hs
+++ dice.hs
@@ -1,26 +1,50 @@
 import Infer
 import Text.Printf
 
-main = do
-  -- Bag of dice
-  let bag = [(12, equally [1..4]), (12, equally [1..6]),
-             (12, equally [1..8]), (16, equally [0..9]),
-             (17, equally [1..12]), (17, equally [1..20])]
+-- Data types specific to solving 'Dice' problems
+data Die = D4 | D6 | D8 | D10 | D12 | D20 deriving (Show)
 
+dieDist :: Die -> P Integer
+dieDist D4 = equally [1..4]
+dieDist D6 = equally [1..6]
+dieDist D8 = equally [1..8]
+dieDist D10 = equally [0..9]
+dieDist D12 = equally [1..12]
+dieDist D20 = equally [1..20]
+
+main = do
+  -- Bag of dice as a literal object
+  let bagLiteral = [(D4, 12), (D6, 12), (D8, 12), (D10, 16),
+                    (D12, 17), (D20, 17)]                   
+  print bagLiteral
+  
   -- Now we turn this representation into a probability dist
-  let bagDist = normWeighted bag
-  --print bagDist
+  let bagDist = normWeighted bagLiteral
+  print bagDist
+  {-
+  let bag = [(equally [1..4], 12), (equally [1..6], 12),
+             (equally [1..8], 12), (equally [0..9], 16),
+             (equally [1..12], 17), (equally [1..20], 17)]
+             -}
+
   let d0 = equally [1..6]
   print d0
   let d1 = equally [1..8]
   print d1
-  let o1 = outcomeProb d1 9
+  let o1 = outcomeProb d1 7
   print o1
-  let e1 = eventProb (> 11) d0
+  let e1 = eventProb (<= 4) d0
   print e1
+
   let exp1 = expected (\x -> x) d0
   print exp1
-  let sup = support bagDist
-  print sup
+  --let sup = support bagDist
+  --print sup
   let d2 = pmap (\x -> (x * 3.14)) d0
   print d2
+  let d3 = pfilter (\x -> (x <= 7)) d1
+  print d3
+  let b1 = bindx bagDist dieDist
+  print b1
+
+
